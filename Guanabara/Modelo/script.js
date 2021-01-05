58a33fee9659cca61f14e599fc96bf376294228a
Luciano Junior <engluciano.junior@gmail.com>
2021-01-05 02:47:17 -0300

    Finalizando exercício site 1

--- Guanabara/Modelo/script.js
+++ Guanabara/Modelo/script.js
@@ -1,14 +1,22 @@
+
 function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagen')
     var data = new Date()
     var hora = data.getHours()
+    //var hora = 21
     msg.innerHTML = `Agora são ${hora} Horas.`
     if (hora >= 0 && hora < 12) {
     //Bom dia
-    } else if (hora >= 12 && hora < 18 ){
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#D5D6DB'
+    } else if (hora >= 12 && hora <= 18 ){
     //Boa Tarde
+        imagen.src = 'fototarde.png'
+        document.body.style.background = '#FB6F01'
     } else {
     //Boa Noite
+        imagen.src = 'fotonoite.png'
+        document.body.style.background = '#357DB7'
     }
 }
