d3c6ca2f70228b731f0d8d9bfd599d0c7ad43150
Luciano Junior <engluciano.junior@gmail.com>
2021-01-06 02:49:17 -0300

    update

--- Guanabara/Modelo3/script.js
+++ Guanabara/Modelo3/script.js
@@ -10,6 +10,45 @@ function verificar() {
     } else {
         //window.alert('Ok')
         var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        //res.innerHTML = `Idade Calculada : ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade <= 10) {
+                //criança
+                img.setAttribute('src','bebemenino.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src','jovemhomem.png')
+            } else if (idade < 50) {
+                    //adulto
+                img.setAttribute('src','adultohomen.png')
+            } else {
+                //Idoso
+                img.setAttribute('src','senhorhomen.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade <= 10) {
+                //criança
+                img.setAttribute('src','meninanova.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src','meninajoven.png')
+            } else if (idade < 50) {
+                    //adulto
+                img.setAttribute('src','adultamulher.png')
+            } else {
+                //Idoso
+                img.setAttribute('src','semhoramulher.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com idade de ${idade} anos`
+        res.appendChild(img)
     }
 
 }
