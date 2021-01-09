21e458ca8c9733020307a1970a7910b9c44038eb
Luciano Junior <engluciano.junior@gmail.com>
2021-01-09 04:57:37 -0300

    ex15.3

---
+++ aula15.3/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5,8,4]
+num[3] = 6 //colocar em certo lugar
+num.push(7) //colocar lugar aleatório
+num.length // comprimento do vetor
+num.sort() //Ordenar os números
+
+console.log(num)
+console.log(`O Vetor tem ${num.length} posições`)
+console.log(` o Vetor tem ${num} elementos`)
