32dde5cadce1c0773891c8898a1d027dd94341e8
Luciano Junior <engluciano.junior@gmail.com>
2021-01-09 05:16:19 -0300

    update

--- aula15.3/ambiente2.js
+++ aula15.3/ambiente2.js
@@ -6,7 +6,17 @@
 } */
 
 let  valores = [8, 1, 8, 4, 8, 11]
+valores.sort
 
 for (let pos = 0; pos < valores.length; pos++) {
     console.log(`A posição: ${pos} tem o valor ${valores[pos]}`)
+}
+
+/* for (let pos in num) {
+    console.log(num[pos])
+}*/
+
+for (let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+
 }
