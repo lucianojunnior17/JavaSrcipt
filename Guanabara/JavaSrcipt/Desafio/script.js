dee44f21a29c6de85e9a0dfa083599af60c894fe
Luciano Junior <engluciano.junior@gmail.com>
2021-01-11 03:37:06 -0300

    commit

--- Guanabara/JavaSrcipt/Desafio/script.js
+++ Guanabara/JavaSrcipt/Desafio/script.js
@@ -1,6 +1,6 @@
 let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
-let res = document.querySelector('div#result')
+let res = document.querySelector('div#res')
 let valores = []
 
 function isNumero(n) {
@@ -22,8 +22,25 @@ function inlista(n, l) {
 
 function calcular() {
     if (isNumero(num.value)  && !inlista(num.value, valores)) {
+        valores.push(Number(num.value)) 
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} Adicionado`
+        lista.appendChild(item)
                 
     } else {
         window.alert('Valor Invalido')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicone Valores por favor')
+    } else {
+        let tot = valores.length
+
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ap todo foram ${tot} valores adicionados </p>`
     }
 }
