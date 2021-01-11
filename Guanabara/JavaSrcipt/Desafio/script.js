fcd4f19c0e3cb18a6157c57be3b81fcba594434c
Luciano Junior <engluciano.junior@gmail.com>
2021-01-11 02:57:30 -0300

    update

---
+++ Guanabara/JavaSrcipt/Desafio/script.js
@@ -0,0 +1,31 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#result')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inlista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }    
+
+}
+
+function calcular() {
+    if (isNumero(num.value)  && !inlista(num.value, valores)) {
+                
+    } else {
+        window.alert('Valor Invalido')
+    }
+
+
+}
